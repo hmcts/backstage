ad010e942086e2a3ec983142da62a5d1243cceb0
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-12-01 10:33:25 +0000

    Fix setting page

--- packages/app/src/plugins.ts
+++ packages/app/src/plugins.ts
@@ -7,3 +7,4 @@ export { plugin as TechDocsPlugin } from '@backstage/plugin-techdocs';
 export { plugin as Explore } from '@backstage/plugin-explore';
 export { plugin as TechRadar } from '@backstage/plugin-tech-radar';
 export { plugin as GithubActions } from '@backstage/plugin-github-actions';
+export { plugin as UserSettings } from '@backstage/plugin-user-settings';
