8127a346564a4f97d9ac56cfe5fb7454cb6aa155
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-12-01 16:18:11 +0000

    fix indentation

--- packages/backend/src/types.ts
+++ packages/backend/src/types.ts
@@ -2,8 +2,8 @@ import { Logger } from 'winston';
 import { Config } from '@backstage/config';
 import {
   PluginDatabaseManager,
- PluginEndpointDiscovery,
- UrlReader,
+  PluginEndpointDiscovery,
+  UrlReader,
 } from '@backstage/backend-common';
 
 export type PluginEnvironment = {
