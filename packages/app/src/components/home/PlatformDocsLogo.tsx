253630c62e5bc7621de88ccd0c7e036b571af838
Tim Jacomb <21194782+timja@users.noreply.github.com>
2022-08-22 14:41:35 +0100

    Improve links (#53)

---
+++ packages/app/src/components/home/PlatformDocsLogo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Logo from './platform-docs-logo.svg'
+
+export const PlatformDocsLogo = () => {
+
+    return (
+        <img width="auto" height="40" src={Logo} alt="React Logo" />
+    );
+};
---
+++ packages/app/src/components/home/VPNIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Logo from './vpn-logo.svg'
+
+export const VPNLogo = () => {
+
+    return (
+        <img width="auto" height="50" src={Logo} alt="React Logo" />
+    );
+};
