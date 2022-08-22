1f3cbbd98171403416f5840d4a435450433e272d
Tim Jacomb <tim.jacomb@hmcts.net>
2022-08-22 09:35:35 +0100

    Fix lint more

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navigate, Route} from 'react-router';
+import {Route} from 'react-router';
 import {apiDocsPlugin, ApiExplorerPage} from '@backstage/plugin-api-docs';
 import {
     CatalogEntityPage,
