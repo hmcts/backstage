599b616be22c6af59603ea76a0c300dcc87d1cb1
Marty Fox <martin.fox1@justice.gov.uk>
2024-05-16 09:59:38 +0100

    adding roadie plugin code

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -2,13 +2,54 @@ import { CatalogClient } from '@backstage/catalog-client';
 import { createRouter } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
-
+import { ScmIntegrations } from '@backstage/integration';
+import { createBuiltinActions } from '@backstage/plugin-scaffolder-backend';
+import {
+    createZipAction,
+    createSleepAction,
+    createWriteFileAction,
+    createAppendFileAction,
+    createMergeJSONAction,
+    createMergeAction,
+    createParseFileAction,
+    createSerializeYamlAction,
+    createSerializeJsonAction,
+    createJSONataAction,
+    createYamlJSONataTransformAction,
+    createJsonJSONataTransformAction,
+    } from '@roadiehq/scaffolder-backend-module-utils';
+ 
 export default async function createPlugin(
     env: PluginEnvironment,
 ): Promise<Router> {
   const catalogClient = new CatalogClient({
     discoveryApi: env.discovery,
   });
+
+  const integrations = ScmIntegrations.fromConfig(env.config);
+
+  const builtInActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    config: env.config,
+    reader: env.reader,
+  });
+
+  const actions = [
+    ...builtInActions, 
+    createZipAction(),
+    createSleepAction(),
+    createWriteFileAction(),
+    createAppendFileAction(),
+    createMergeJSONAction({}),
+    createMergeAction(),
+    createParseFileAction(),
+    createSerializeYamlAction(),
+    createSerializeJsonAction(),
+    createJSONataAction(),
+    createYamlJSONataTransformAction(),
+    createJsonJSONataTransformAction(),
+];
 
   return await createRouter({
     logger: env.logger,
@@ -17,5 +58,6 @@ export default async function createPlugin(
     reader: env.reader,
     catalogClient,
     identity: env.identity,
+    actions
   });
 }
