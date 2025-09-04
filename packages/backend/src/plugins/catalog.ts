65eae8579eca9a970030fb17fc33fc14a40c7a26
Jack Maloney <jack.maloney1@hmcts.net>
2025-09-04 16:50:22 +0100

    Update catalog logger

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,5 +1,8 @@
 import { MicrosoftGraphOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-msgraph';
-import { createBackendModule } from '@backstage/backend-plugin-api';
+import {
+  createBackendModule,
+  coreServices,
+} from '@backstage/backend-plugin-api';
 import { catalogProcessingExtensionPoint } from '@backstage/plugin-catalog-node/alpha';
 
 const catalogModuleCustomExtensions = createBackendModule({
@@ -9,13 +12,14 @@ const catalogModuleCustomExtensions = createBackendModule({
     env.registerInit({
       deps: {
         catalog: catalogProcessingExtensionPoint,
+        logger: coreServices.logger,
       },
       async init({ catalog }) {
         catalog.addEntityProvider(
           MicrosoftGraphOrgEntityProvider.fromConfig(env.config, {
             id: 'production', // uniquely identify this provider
             target: 'https://graph.microsoft.com/v1.0',
-            logger: env.logger,
+            logger,
             schedule: env.scheduler.createScheduledTaskRunner({
               frequency: { hours: 1 },
               timeout: { minutes: 50 },
